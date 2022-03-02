91b02599f6efe65e1468c72a18caf08b3d9851f5
girvenavery2022 <girvenavery@gmail.com>
2022-03-02 16:33:00 -0500

    dynamic params and lookup transform

--- include/isc_nav/pure_pursuit_node.hpp
+++ include/isc_nav/pure_pursuit_node.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
 #include "isc_nav/pure_pursuit.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/path.hpp"
 #include <geometry_msgs/msg/twist.hpp>
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 #include <tf2/exceptions.h>
+#include <tf2/time.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
@@ -33,13 +40,17 @@ private:
     // TF
     std::shared_ptr<tf2_ros::TransformListener> transform_listener;
     std::unique_ptr<tf2_ros::Buffer> tf_buffer;
+    float tf_timeout;
 
     void path_callback(const nav_msgs::msg::Path::SharedPtr msg);
+    void param_update();
     Path to_path(const nav_msgs::msg::Path::SharedPtr msg);
     Point3D to_point3d(const geometry_msgs::msg::PoseStamped pose);
     Point3D get_pose();
 
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_subscription;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr velocity_publisher;
+    rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr carrot_publisher;
+    rclcpp::TimerBase::SharedPtr param_update_timer;
 };
 }  // namespace isc_nav
--- src/pure_pursuit_node.cpp
+++ src/pure_pursuit_node.cpp
@@ -2,11 +2,13 @@
 
 namespace isc_nav
 {
+using namespace std::chrono_literals;
+
 PurePursuitNode::PurePursuitNode(rclcpp::NodeOptions options)
   : Node("pure_pursuit", options), m_tracker(m_path, m_lookahead_distance), m_path_is_initialized(false)
 {
   path_subscription = this->create_subscription<nav_msgs::msg::Path>(
-    "/path", 10,
+    "/navigation/path", 10,
     std::bind(&PurePursuitNode::path_callback, this, std::placeholders::_1));
 
   velocity_publisher = this->create_publisher<geometry_msgs::msg::Twist>(
@@ -15,8 +17,21 @@ PurePursuitNode::PurePursuitNode(rclcpp::NodeOptions options)
   this->declare_parameter<float>("m_lookahead_distance", 0.1f);
   this->declare_parameter<std::string>("robot_frame", "base_footprint");
   this->declare_parameter<std::string>("map_frame", "map");
-  this->declare_parameter<float>("m_lookahead_distance", 0.1f);
   this->declare_parameter<bool>("m_path_is_initialized", false);
+  this->declare_parameter("tf_timeout", 0.03);
+
+  tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  transform_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
+
+  param_update_timer = this->create_wall_timer(
+    1000ms, std::bind(&PurePursuitNode::param_update, this)
+  );
+}
+
+void PurePursuitNode::param_update()
+{
+  this->get_parameter("m_lookahead_distance", m_lookahead_distance);
+  this->get_parameter("tf_timeout", tf_timeout);
 }
 
 void PurePursuitNode::path_callback(const nav_msgs::msg::Path::SharedPtr msg)
@@ -48,7 +63,18 @@ Point3D PurePursuitNode::to_point3d(const geometry_msgs::msg::PoseStamped pose)
 
 Point3D PurePursuitNode::get_pose()
 {
-
+  Point3D pose;
+  geometry_msgs::msg::TransformStamped transformStamped;
+  try {
+    transformStamped = tf_buffer->lookupTransform(map_frame, robot_frame, this->get_clock()->now(),
+      rclcpp::Duration::from_seconds(tf_timeout));
+  } 
+  catch (tf2::TransformException & ex) {
+    RCLCPP_INFO(
+    this->get_logger(), "Could not transform %s to %s: %s",
+    map_frame.c_str(), robot_frame.c_str(), ex.what());
+  }
+  return pose;
 }
 
 }  // namespace isc_nav
